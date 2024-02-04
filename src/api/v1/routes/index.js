fdddfeef4c860781224d31c6dc0b749809cf0d88
Almer Tampus <almerflorestampus@gmail.com>
2024-02-04 18:12:39 +0800

    Setup routes config

---
+++ src/api/v1/routes/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const { createUrl, findUrl } = require('../services/Url');
+const isInvalidUrl = require('../../../utils/dns-lookup');
+
+const router = new express.Router();
+
+router.post('/api/shorturl', async (req, res) => {
+	try {
+		const { url } = req.body;
+
+		if (isInvalidUrl(url)) throw new Error();
+
+		const { original_url, short_url } = await createUrl(url);
+
+		res.json({ original_url, short_url });
+	} catch (err) {
+		console.log(err);
+
+		res.json({ error: 'invalid url' });
+	}
+});
+
+router.get('/api/shorturl/:url?', async (req, res) => {
+	try {
+		const { url } = req.params;
+
+		if (!url) throw new Error();
+
+		const originalUrl = await findUrl(url);
+
+		if (!originalUrl) throw new Error();
+
+		res.redirect(originalUrl);
+	} catch (err) {
+		console.log(err);
+
+		res.json({ error: 'invalid url' });
+	}
+});
+
+module.exports = router;
